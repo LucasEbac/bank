62e9621945bff38cfe52646edd211ad2fb7c4c9e
Lucas santos <Lucass.santos2015@outlook.com>
2023-08-01 19:56:04 -0300

    ajustando a crase

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('form-deposito');
 
 function validaNome(nomeCompleto){
-    const nomeComArray= nomeCompleto.split('');
-    return nomeComArray.length >=2;
+    const nomeComoArray= nomeCompleto.split(' ');
+    return nomeComoArray.length >=2;
 }
 
 form.addEventListener('submit' , function(e){
@@ -12,15 +12,15 @@ form.addEventListener('submit' , function(e){
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = 'motante de:' ${valorDepoaito.value} 'deposito realizado para o cliente' ${nomeBeneficiario.value} - 'conta' ${numeroContaBeneficiario.value};
-   
-    formValido= validaNome(nomeBeneficiario.value)
+    const mensagemSucesso = `montante de: ${valorDeposito.value} depositado para o cliente ${nomeBeneficiario.value} -conta ${numeroContaBeneficiario.value}`;
+
+    formValido= validaNome(nomeBeneficiario.value);
     if  (formValido) {
         alert(mensagemSucesso);
         
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
-        valorDepoaito.value = '';
+        valorDeposito.value = '';
 
     } else {
         alert("o nome não esta completo");
